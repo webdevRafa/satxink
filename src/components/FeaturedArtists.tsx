55cfe63b9079ae57041aae1af920638b38cead3c
Rafael Castro <ralphc1992@live.com>
2025-06-19 09:54:37 -0500

    each div indiv

--- src/components/FeaturedArtists.tsx
+++ src/components/FeaturedArtists.tsx
@@ -24,15 +24,13 @@ const artists = [
 
 export const FeaturedArtists = () => {
   return (
-    <section
-      data-aos="fade-up"
-      className="px-4 py-12 max-w-6xl mx-auto bg-[#121212]"
-    >
+    <section className="px-4 py-12 max-w-6xl mx-auto bg-[#121212]">
       <h2>Featured Artists</h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 mt-6">
         {artists.map((artist, index) => (
           <div
+            data-aos="fade-up"
             key={index}
             className="bg-[#1c1c1c] text-white rounded-xl overflow-hidden shadow-md"
           >
