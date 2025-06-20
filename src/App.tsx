f135fd937e94d0ee15ca3e384d60d857947790f9
Rafael Castro <ralphc1992@live.com>
2025-06-20 15:07:15 -0500

    new logo

--- src/App.tsx
+++ src/App.tsx
@@ -29,20 +29,22 @@ function App() {
 
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/clients" element={<Clients />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/artists" element={<ArtistsPage />} />
-        <Route path="/client-posts" element={<ClientPostsPage />} />
-        <Route path="/dev-add-docs" element={<DevAddDocs />} />
-        <Route path="/login-page" element={<LoginPage />} />
-        <Route path="/artists/:id" element={<ArtistProfilePage />} />
-      </Routes>
+      <div className="overflow-hidden">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/clients" element={<Clients />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/artists" element={<ArtistsPage />} />
+          <Route path="/client-posts" element={<ClientPostsPage />} />
+          <Route path="/dev-add-docs" element={<DevAddDocs />} />
+          <Route path="/login-page" element={<LoginPage />} />
+          <Route path="/artists/:id" element={<ArtistProfilePage />} />
+        </Routes>
 
-      <Footer />
+        <Footer />
+      </div>
     </>
   );
 }
